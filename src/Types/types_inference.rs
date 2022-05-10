c11b95e8fd071dc0fede51b517bdd87f3f147fe8
Rajeeb Kumar Malik <97892873+rajeebkm@users.noreply.github.com>
2022-05-11 02:10:08 +0530

    Update Type Inference added

--- src/Types/types_inference.rs
+++ src/Types/types_inference.rs
@@ -1,4 +1,4 @@
-//-------------------Inference-------------------//
+//-----------------------------------------------------------Inference----------------------------------------------------------------//
 
 //The type inference engine is pretty smart. It does more than looking at the type of the value expression during an initialization. It also looks at how the variable is used afterwards to infer its type. Here's an advanced example of type inference:
 
@@ -14,9 +14,17 @@ fn main() {
     // Insert `elem` in the vector.
     vec.push(elem);
     // Aha! Now the compiler knows that `vec` is a vector of `u8`s (`Vec<u8>`)
-    // TODO ^ Try commenting out the `vec.push(elem)` line
+    let elem2=6u16 as u8;
+    vec.push(elem2);
+
+    let mut vec2=Vec::new();
+    let elem3=7usize;
+    let elem4=elem3+1usize;
+    vec2.push(elem3);
+    vec2.push(elem4);
 
     println!("{:?}", vec);
+    println!("{:?}", vec2);
 }
 
 //No type annotation of variables was needed, the compiler is happy and so is the programmer!
