e23e1c4c9b6df33972ca3b5f3b6a8149474a728e
rajeebkm <rkm2541997@gmail.com>
2022-05-11 14:36:07 +0530

    Update conversion try_from & try_into added

--- src/Conversion/conversion-try_from&try_into.rs
+++ src/Conversion/conversion-try_from&try_into.rs
@@ -1,3 +1,5 @@
+//Similar to From and Into, TryFrom and TryInto are generic traits for converting between types. Unlike From/Into, the TryFrom/TryInto traits are used for fallible conversions, and as such, return Results.
+
 use std::convert::TryFrom;
 use std::convert::TryInto;
 
